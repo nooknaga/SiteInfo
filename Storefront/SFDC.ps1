a20a07773995278fde383223a54897761fc33799
Nagaraj Nookala <nookala.nagaraj@gmail.com>
2016-07-03 20:11:28 +0530

    Adding sample test file

---
+++ Storefront/SFDC.ps1
@@ -0,0 +1,10 @@
+﻿$cmd = {
+$text = 'Hello World'
+$text | Add-Content D:\1234.txt
+$env:StoreFront1 | Add-Content D:\1234.txt
+$env:StoreFront2 | Add-Content D:\1234.txt
+$env:DeliveryController1 | Add-Content D:\1234.txt
+$env:DeliveryController2 | Add-Content D:\1234.txt
+}
+Invoke-Command -ScriptBlock $cmd
+
