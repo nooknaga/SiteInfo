e4c0f61bceff26d31a837687e6c8e8003d0f1ef9
Nagaraj Nookala <nookala.nagaraj@gmail.com>
2016-07-07 22:32:45 +0530

    modified sfdc1

--- Storefront/SFDC.ps1
+++ Storefront/SFDC.ps1
@@ -1,10 +1,33 @@
-﻿$cmd = {
+﻿param(
+[parameter(Mandatory=$True)]
+[String]$Site1StoreFront1,
+[parameter(Mandatory=$True)]
+[String]$Site1StoreFront2,
+[parameter(Mandatory=$True)]
+[String]$Site1DeliveryController1,
+[parameter(Mandatory=$True)]
+[String]$Site1DeliveryController2,
+[parameter(Mandatory=$True)]
+[String]$Site2StoreFront1,
+[parameter(Mandatory=$True)]
+[String]$Site2StoreFront2,
+[parameter(Mandatory=$True)]
+[String]$Site2DeliveryController1,
+[parameter(Mandatory=$True)]
+[String]$Site2DeliveryController2
+)
+
+$cmd = {
 $text = 'Hello World'
 $text | Add-Content D:\1234.txt
-$env:StoreFront1 | Add-Content D:\1234.txt
-$env:StoreFront2 | Add-Content D:\1234.txt
-$env:DeliveryController1 | Add-Content D:\1234.txt
-$env:DeliveryController2 | Add-Content D:\1234.txt
+$Site1StoreFront1 | Add-Content D:\1234.txt
+$Site1StoreFront2 | Add-Content D:\1234.txt
+$Site1DeliveryController1 | Add-Content D:\1234.txt
+$Site1DeliveryController2 | Add-Content D:\1234.txt
+$Site2StoreFront1 | Add-Content D:\1234.txt
+$Site2StoreFront2 | Add-Content D:\1234.txt
+$Site2DeliveryController1 | Add-Content D:\1234.txt
+$Site2DeliveryController2 | Add-Content D:\1234.txt
 }
 Invoke-Command -ScriptBlock $cmd
 
