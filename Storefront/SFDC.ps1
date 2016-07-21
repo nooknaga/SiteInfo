86866019acc0fd9ee96b8a62d29e7647df891851
Nagaraj Nookala <nookala.nagaraj@gmail.com>
2016-07-22 00:01:31 +0530

    Testing

--- Storefront/SFDC.ps1
+++ Storefront/SFDC.ps1
@@ -1,20 +1,22 @@
 ﻿param(
 [parameter(Mandatory=$True)]
+[String]$FirmID,
+[parameter(Mandatory=$True)]
 [String]$Site1StoreFront1,
-[parameter(Mandatory=$True)]
-[String]$Site1StoreFront2,
-[parameter(Mandatory=$True)]
-[String]$Site1DeliveryController1,
-[parameter(Mandatory=$True)]
-[String]$Site1DeliveryController2,
+#[parameter(Mandatory=$True)]
+#[String]$Site1StoreFront2,
+#[parameter(Mandatory=$True)]
+#[String]$Site1DeliveryController1,
+#[parameter(Mandatory=$True)]
+#[String]$Site1DeliveryController2,
 [parameter(Mandatory=$True)]
 [String]$Site2StoreFront1,
-[parameter(Mandatory=$True)]
-[String]$Site2StoreFront2,
-[parameter(Mandatory=$True)]
-[String]$Site2DeliveryController1,
-[parameter(Mandatory=$True)]
-[String]$Site2DeliveryController2,
+#[parameter(Mandatory=$True)]
+#[String]$Site2StoreFront2,
+#[parameter(Mandatory=$True)]
+#[String]$Site2DeliveryController1,
+#[parameter(Mandatory=$True)]
+#[String]$Site2DeliveryController2,
 [parameter(Mandatory=$True)]
 [String]$Site1CitrixSiteName,
 [parameter (Mandatory=$True)]
@@ -22,16 +24,17 @@
 )
 
 $cmd = {
-$Site1StoreFront1 | Add-Content C:\1234.txt
-$Site1StoreFront2 | Add-Content C:\1234.txt
-$Site1DeliveryController1 | Add-Content C:\1234.txt
-$Site1DeliveryController2 | Add-Content C:\1234.txt
-$Site2StoreFront1 | Add-Content C:\1234.txt
-$Site2StoreFront2 | Add-Content C:\1234.txt
-$Site2DeliveryController1 | Add-Content C:\1234.txt
-$Site2DeliveryController2 | Add-Content C:\1234.txt
-$Site1CitrixSiteName | Add-Content C:\1234.txt
-$Site2CitrixSiteName | Add-Content C:\1234.txt
+$FirmID | Add-Content D:\1234.txt
+$Site1StoreFront1 | Add-Content D:\1234.txt
+#$Site1StoreFront2 | Add-Content C:\1234.txt
+#$Site1DeliveryController1 | Add-Content C:\1234.txt
+#$Site1DeliveryController2 | Add-Content C:\1234.txt
+$Site2StoreFront1 | Add-Content D:\1234.txt
+#$Site2StoreFront2 | Add-Content C:\1234.txt
+#$Site2DeliveryController1 | Add-Content C:\1234.txt
+#$Site2DeliveryController2 | Add-Content C:\1234.txt
+$Site1CitrixSiteName | Add-Content D:\1234.txt
+$Site2CitrixSiteName | Add-Content D:\1234.txt
 }
 Invoke-Command -ScriptBlock $cmd
 
